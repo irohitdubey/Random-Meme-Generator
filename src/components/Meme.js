62b76c44cbdc64c2a6298964363aeb08feecb2f7
irohitdubey <ceo.rohitdubey@gmail.com>
2022-06-12 11:49:17 +0530

    Random-Meme-Generator

---
+++ src/components/Meme.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react"
+import memesData from "../MemesData"
+
+export default function Meme() {
+    const [memeImage, setMemeImage] = useState("")
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+    
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={memeImage} alt 
+            = "" className="meme--image" />
+        </main>
+    )
+}
